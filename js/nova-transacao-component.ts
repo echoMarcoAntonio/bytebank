22f3972293d024851b922ee93dd0f6c4eae483a0
echoMarcoAntonio <m.douglas.enterprises@outlook.com.br>
2024-07-06 20:38:13 -0300

    nova-transacao-funcional
    Componente nova-transacao-component modificado para TypeScript. Componente funcional

--- js/nova-transacao-component.ts
+++ js/nova-transacao-component.ts
@@ -1,39 +1,46 @@
-var saldo = 3500;
-var elementoSaldo = document.querySelector(".saldo-valor .valor");
+let saldo = 3500;
+
+const elementoSaldo = document.querySelector(".saldo-valor .valor");
 if (elementoSaldo != null) {
     elementoSaldo.textContent = saldo.toString();
 }
-var elementoFormulario = document.querySelector(".block-nova-transacao form");
+
+
+const elementoFormulario = document.querySelector(".block-nova-transacao form") as HTMLFormElement;
 elementoFormulario.addEventListener("submit", function (event) {
     event.preventDefault(); // previne comportamento padrão: recarregar a página.
     if (!elementoFormulario.checkValidity()) {
         alert("Por favor, preencha todos os campos da transação!");
         return;
     }
-    var inputTipoTransacao = document.querySelector("#tipoTransacao");
-    var inputValor = elementoFormulario.querySelector("#valor");
-    var inputData = elementoFormulario.querySelector("#data");
-    var tipoTransacao = inputTipoTransacao.value;
-    var valor = inputValor.valueAsNumber;
-    var data = new Date(inputData.value);
+
+    const inputTipoTransacao = document.querySelector("#tipoTransacao") as HTMLSelectElement;
+    const inputValor = elementoFormulario.querySelector("#valor") as HTMLInputElement;
+    const inputData = elementoFormulario.querySelector("#data") as HTMLInputElement;
+
+    let tipoTransacao: string = inputTipoTransacao.value;
+    let valor: number = inputValor.valueAsNumber;
+    let data: Date = new Date(inputData.value);
+
     if (tipoTransacao == "Depósito") {
         saldo += valor;
-    }
-    else if (tipoTransacao == "Transferência" || tipoTransacao == "Pagamento de Boleto") {
+    } else if (tipoTransacao == "Transferência" || tipoTransacao == "Pagamento de Boleto") {
         saldo -= valor;
-    }
-    else {
+    } else {
         alert("Tipo de transação é inválido!");
         return;
     }
+
     if (elementoSaldo != null) {
         elementoSaldo.textContent = saldo.toString();
     }
-    var novaTransacao = {
+
+    const novaTransacao = {
         tipoTransacao: tipoTransacao,
         valor: valor,
         data: data
-    };
+    }
+
     console.log(novaTransacao);
     elementoFormulario.reset();
 });
